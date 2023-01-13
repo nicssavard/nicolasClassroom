2c0e539f05b4b7353313e87ba9882ef25dfc435e
nicolas <nicssavard@gmail.com>
2023-01-13 18:15:06 +0800

    added Class to db

--- src/pages/subjects.tsx
+++ src/pages/subjects.tsx
@@ -1,15 +1,29 @@
+//import { GetServerSideProps } from "next";
 import ListSubject from "../components/subjects/ListSubjects";
 
 import { api } from "../utils/api";
 
+// type Props = {
+//   subjects: Subject[] | undefined;
+// }
 export default function Subjects(): JSX.Element {
   const { data: subjects } = api.subjects.getSubjects.useQuery();
-
+  console.log(subjects);
   if (subjects) {
     return <ListSubject subjects={subjects}></ListSubject>;
   }
   return <div>loading</div>;
 }
+
+// export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+//   const { data: subjects } = api.subjects.getSubjects.useQuery();
+  
+  
+//   const _props: any = {
+//     subjects: subjects,
+//   }
+//   return { props: _props }
+// };
 
 // export async function getStaticProps() {
 //   const { data: subjects } = api.subjects.getSubjects.useQuery();
